608c7adc4da967862da93c09626edf21e1ddf196
Chrystian Paim <chrystiopaim@gmail.com>
2024-03-07 16:52:58 -0300

    Ajustes vm

--- ec2/main.tf
+++ ec2/main.tf
@@ -7,21 +7,21 @@ provider "aws" {
 #Security Group Load Balancer
 
 data "aws_vpc" "vpc_id" {
-  id = "vpc-083448a38ca29ae3a"
+  id = "vpc-0cf3b31ebba781f07"
 }
 
 data "aws_subnet" "subnet1" {
-  id = "subnet-080a288826a878f85"
+  id = "subnet-02ec4e9fce4b856e1"
 }
 
 data "aws_subnet" "subnet2" {
-  id = "subnet-0db4e5df47acec16c"
+  id = "subnet-052bc856af02547eb"
 }
 
 resource "aws_security_group" "alb" {
   name        = "alb-sg"
   description = "LoadBalancerSG"
-  vpc_id      = data.aws_vpc.vpc_id
+  vpc_id      = data.aws_vpc.vpc_id.id
 
   ingress {
     from_port   = 80
@@ -46,7 +46,7 @@ resource "aws_security_group" "alb" {
 resource "aws_lb" "alb" {
   name            = "DeveqiALB"
   security_groups = [aws_security_group.alb.id]
-  subnets         = [data.aws_subnet.subnet1, data.aws_subnet.subnet2]
+  subnets         = [data.aws_subnet.subnet1.id, data.aws_subnet.subnet2.id]
 
 }
 
@@ -55,7 +55,7 @@ resource "aws_lb_target_group" "lbtg" {
   name     = "ALB-TG"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.vpc_id
+  vpc_id   = data.aws_vpc.vpc_id.id
 
   health_check {
     path              = "/"
@@ -79,7 +79,7 @@ resource "aws_lb_listener" "front_end" {
 resource "aws_security_group" "autoscaling" {
   name        = "autoscaling"
   description = "Security group auto scaling"
-  vpc_id      = data.aws_vpc.vpc_id
+  vpc_id      = data.aws_vpc.vpc_id.id
 
   ingress {
     from_port   = 22
@@ -110,6 +110,7 @@ resource "aws_security_group" "autoscaling" {
 
 #EC2 AUTO SCALING
 
+
 resource "aws_launch_template" "launchtp" {
   name_prefix = "deveqi"
   image_id = var.ami_id
@@ -128,4 +129,20 @@ resource "aws_launch_template" "launchtp" {
      delete_on_termination = true
      security_groups = [aws_security_group.autoscaling.id]
   }
+}
+
+resource "aws_instance" "vm" {
+  ami                         = var.ami_id
+  instance_type               = var.instance-type
+  key_name                    = aws_key_pair.key.key_name
+  subnet_id                   = data.aws_subnet.subnet1.id
+  vpc_security_group_ids      = [aws_security_group.autoscaling.id, aws_security_group.alb.id]
+  associate_public_ip_address = true
+
+  tags = {
+
+    name = "deveqi-vm"
+  }
+
+
 }
